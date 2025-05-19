eed85870cda2575c269025a8e0fefc0272ccd3ae
YunSang123 <fgf7404@naver.com>
2025-05-19 19:32:48 +0200

    Feature Done Done - 1. Subscribed pointcloud2 topic from zed_camera node. Transformed data   type from pointcloud2 to pointcloud - 2. Removed nan pointcloud - 3. Frame Transformation from zed_camera_left frame to rover   base_footprint frame Todo - 4. Extract sparse and dense heightmap - 5. Publish heightmap topic

--- src/pointcloud_converter_node.cpp
+++ src/pointcloud_converter_node.cpp
@@ -7,6 +7,10 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <pcl/common/transforms.h>
+
+#include <cmath>  // for std::isnan
+#include <Eigen/Dense>
 
 class PointCloudConverter : public rclcpp::Node
 {
@@ -18,18 +22,39 @@ public:
       std::bind(&PointCloudConverter::callback, this, std::placeholders::_1));
 
     pub_ = this->create_publisher<sensor_msgs::msg::PointCloud>("/output_pointcloud", 10);
+    last_time_ = this->now();  // 초기화
   }
 
 private:
+  rclcpp::Time last_time_;
   void callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
   {
+    auto current_time = this->now();
+    double time_diff_sec = (current_time - last_time_).seconds();
+    last_time_ = current_time;
+
     pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
     pcl::fromROSMsg(*msg, pcl_cloud);
 
+    // 1. 좌표변환 행렬 정의
+    Eigen::Matrix4f transform;
+    transform << 1, 0, 0,  0.165,
+                 0, 1, 0,  0.000,
+                 0, 0, 1,  0.320,
+                 0, 0, 0,  1;
+
+    // 2. 변환 적용
+    pcl::PointCloud<pcl::PointXYZ> pcl_transformed;
+    pcl::transformPointCloud(pcl_cloud, pcl_transformed, transform);
+    
+    // 3. 변환된 결과를 sensor_msgs::PointCloud로 변환
     sensor_msgs::msg::PointCloud cloud;
     cloud.header = msg->header;
 
-    for (const auto& pt : pcl_cloud.points) {
+    for (const auto& pt : pcl_transformed.points) {
+      if (std::isnan(pt.x)) {
+        continue;  // NaN 포인트는 제외
+      }
       geometry_msgs::msg::Point32 p;
       p.x = pt.x;
       p.y = pt.y;
@@ -38,7 +63,7 @@ private:
     }
 
     pub_->publish(cloud);
-    RCLCPP_INFO(this->get_logger(), "Published legacy PointCloud with %lu points", cloud.points.size());
+    RCLCPP_INFO(this->get_logger(), "Published legacy PointCloud with %lu points | Δt = %.3f Hz", cloud.points.size(), 1/time_diff_sec);
   }
 
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_;
